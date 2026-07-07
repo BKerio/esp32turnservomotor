5e8b3549061951bd6ec69e227d33ce97cb942c0c
BKerio <briankerio48@gmail.com>
2026-07-07 22:19:05 +0300

    new changes

--- servo.cpp
+++ servo.cpp
@@ -1,6 +1,6 @@
 #include <ESP32Servo.h>
 
-Servo myServo;
+Servo myServo;u
 
 // Safe GPIO pins for ESP32 servo: 13, 14, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33
 // Avoid: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15 (used for boot/flash/UART)
