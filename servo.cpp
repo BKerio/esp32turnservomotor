b4eb0410cd4b8ea0226019e7e1b607d88fbe3380
BKerio <briankerio48@gmail.com>
2026-07-17 22:05:07 +0300

    new changes

--- servo.cpp
+++ servo.cpp
@@ -4,7 +4,7 @@ Servo myServo;
 
 // Safe GPIO pins for ESP32 servo: 13, 14, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33
 // Avoid: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15 (used for boot/flash/UART)
-int servoPin = 13;jj
+int servoPin = 13;
 
 void setup() {
   // Allow allocation of all timers for servo
